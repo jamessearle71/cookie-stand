02954afc18074ca79becc4bce88b221bdfe10266
James Searle <jamessearle41@hotmail.co.uk>
2023-03-27 10:58:27 +0100

    update

--- top.js
+++ top.js
@@ -1,22 +1,24 @@
-function yourName(){
-    let user = prompt("What is your name? ")
-    return document.write(user)
+let user;
+
+function yourName() {
+  user = prompt("What is your name?");
+  return document.write(user);
 }
 
-function timeOfDay(){
-    let time = prompt("Hi, " + user + ", what hour is it? (0 - 23)");
+function timeOfDay() {
+  let time = prompt("Hi " + user + ", what hour is it? (0 - 23)");
 
-    let message; 
+  let message;
 
-    if(time < 12){
-        message = "Good morning " + user;
-    } else if(time <= 18){
-        message - "Good afternoon " + user;
-    } else if(time < 24){
-        message = "Good night " + user;
-    } else{
-        message = "That hour does not exist ";
-    }
-    console.log(message);
-    return message
+  if (time < 12) {
+    message = "Good morning, " + user;
+  } else if (time <= 18) {
+    message = "Good afternoon " + user;
+  } else if (time < 24) {
+    message = "Good night " + user;
+  } else {
+    message = "That hour does not exist " + user;
+  }
+  console.log(message);
+  return message;
 }
